875b98b1a42c19125f33537b2327a866834cc869
Abhishek Hiremath <abhishekaf3421@gmail.com>
2025-08-02 11:12:10 +0530

    Add email confirmation for password reset: implement send_confirm_email function and update password reset flow to send confirmation email after password reset

--- routers/password_reset_route.py
+++ routers/password_reset_route.py
@@ -5,7 +5,7 @@ from db import get_db
 from core.security import create_password_access_token,verify_password_reset_token
 from utils.exceptions import APIException
 
-from services.password_reset import send_reset_email
+from services.password_reset import send_reset_email,send_confirm_email
 from models import User
 from hashing import Hash
 
@@ -20,10 +20,11 @@ async def request_password_reset(request: passwordResetRequest,bg:BackgroundTask
         token = create_password_access_token(user.email)
         reset_link = f"http://localhost:8000/reset-password?token={token}"
         bg.add_task(send_reset_email, email_to=user.email, reset_link=reset_link)
+        
     return {"message" : "If the email is registered, a password reset link has been sent to your email."}
 
 @apirouter.post('/reset-password')
-def reset_password(data: passwordResetConfirm,db:Session = Depends(get_db)):
+def reset_password(data: passwordResetConfirm,bg:BackgroundTasks,db:Session = Depends(get_db)):
     email= verify_password_reset_token(data.token)
     if not email:
         raise APIException(400,"Bhai token Invalid hai")
@@ -31,10 +32,11 @@ def reset_password(data: passwordResetConfirm,db:Session = Depends(get_db)):
     if not user:
         raise APIException(404,"Bhai user nahi mila")
     user.password=Hash.bcrypt(data.new_password)
-    print("New hashed password:", user.password)
+    
 
     db.add(user)
     db.commit()
+    bg.add_task(send_confirm_email, email_to=user.email)
     return {"message": "Bhai Congrats! Password reset ho gayaaaaa"}
 
     
--- services/password_reset.py
+++ services/password_reset.py
@@ -20,3 +20,20 @@ async def send_reset_email(email_to:str,reset_link:str):
         subtype="html"
     )
     await fm.send_message(msg)
+    
+async def send_confirm_email(email_to:str):
+    msg=MessageSchema(
+        subject="Password Reset Request",
+        recipients=[email_to],
+        body=f"""
+        Hi there,
+
+        You request for password reset is done
+        If you did not request this, please ignore this email.
+        Best regards,
+        Team Sparsha
+        """,
+        subtype="html"
+    )
+    await fm.send_message(msg)
+    
